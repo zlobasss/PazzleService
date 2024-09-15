7598bc883b197cb1ec42edb2f9b596aaed5875ef
zlobasss <sergey4ik@outlook.com>
2024-09-15 13:31:10 +0700

    Init commit

---
+++ src/main/java/ru/zlobass/pazzleservice/PazzleServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.zlobass.pazzleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazzleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PazzleServiceApplication.class, args);
+    }
+
+}
